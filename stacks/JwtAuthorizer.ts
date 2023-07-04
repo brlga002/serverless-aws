58a28c7b5616353573b9d7f10a917858ab0aa5b1
brlga002 <gabriel.lima1000@gmail.com>
2023-07-03 20:23:04 -0400

    feat: JwtTokenService use JWT_APPLICATION_KEY

--- stacks/JwtAuthorizer.ts
+++ stacks/JwtAuthorizer.ts
@@ -2,13 +2,14 @@ import { ApiAuthorizer, Function, Stack } from 'sst/constructs'
 
 export const JwtAuthorizer = (stack: Stack): ApiAuthorizer => {
   const handler = new Function(stack, 'JwtAuthorizer', {
+    environment: { JWT_APPLICATION_KEY: process.env.JWT_APPLICATION_KEY! },
     handler: 'src/4-framework/auth/LambdaAuthorizer.handler',
   })
 
   return {
     type: 'lambda',
     function: handler,
-    resultsCacheTtl: '3 minutes',
-    identitySource: ['$request.header.Authorization', '$request.header.code'],
+    resultsCacheTtl: '10 minutes',
+    identitySource: ['$request.header.Authorization'],
   }
 }
