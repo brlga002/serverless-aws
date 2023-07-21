861a47e6062cc766ce31cda61dffcf224dc390a0
brlga002 <gabriel.lima1000@gmail.com>
2023-07-21 14:09:17 -0400

    feat: convertedValue ApplicationResult

--- src/0-core/application/result/ApplicationResult.ts
+++ src/0-core/application/result/ApplicationResult.ts
@@ -1,12 +1,22 @@
-export class ApplicationResult<T = any> {
+import { Entity } from '0-core/domain/entities/Entity'
+
+export class ApplicationResult<T = unknown> {
   private constructor(public successCode: number, public content?: T) {}
 
-  static success<T>(value?: T): ApplicationResult<T> {
-    return new ApplicationResult(200, value)
+  static success<T>(value?: Entity | T): ApplicationResult<T> {
+    const convertedValue = value as unknown as Entity<T>
+    if (typeof convertedValue?.toJSON === 'function')
+      return new ApplicationResult(200, convertedValue.toJSON(true))
+
+    return new ApplicationResult(200, value as T)
   }
 
-  static created<T>(value: T): ApplicationResult<T> {
-    return new ApplicationResult(201, value)
+  static created<T>(value: Entity): ApplicationResult<T> {
+    const convertedValue = value as unknown as Entity<T>
+    if (typeof convertedValue?.toJSON === 'function')
+      return new ApplicationResult(201, convertedValue.toJSON(true))
+
+    return new ApplicationResult(201, value as T)
   }
 
   static accepted<T>(value: T): ApplicationResult<T> {
@@ -17,7 +27,7 @@ export class ApplicationResult<T = any> {
     return new ApplicationResult(203, value)
   }
 
-  static noContent<T>(value?: T): ApplicationResult<T> {
-    return new ApplicationResult(204, value)
+  static noContent(): ApplicationResult<boolean> {
+    return new ApplicationResult(204, true)
   }
 }
