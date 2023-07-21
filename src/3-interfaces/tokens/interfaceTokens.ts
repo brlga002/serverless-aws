df39cccf1bd5ff6ba3623bb03ad1363c27ecee52
brlga002 <gabriel.lima1000@gmail.com>
2023-07-21 14:24:41 -0400

    added token

--- src/3-interfaces/tokens/interfaceTokens.ts
+++ src/3-interfaces/tokens/interfaceTokens.ts
@@ -3,6 +3,7 @@ import { APPLICATION_TOKENS } from '2-application/tokens/applicationTokens'
 export const INTERFACE_TOKENS = {
   ...APPLICATION_TOKENS,
   AuthUserController: Symbol.for('AuthUserController'),
+  UpdateUserPasswordController: Symbol.for('UpdateUserPasswordController'),
   UserController: Symbol.for('UserController'),
   TenantController: Symbol.for('TenantController'),
 }
