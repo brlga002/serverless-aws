dbe483140659011df69de04c7abe1a97d3f48b1b
brlga002 <gabriel.lima1000@gmail.com>
2023-07-23 17:48:18 -0400

    fix: remove old files

--- src/2-application/repositories/BeersRepository.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from '0-core/application/repositories/Repository'
-import { Beer, BeerDto } from '1-domain/entities/Beer/Beer'
-
-export interface BeersRepository extends Repository<Beer, BeerDto> {
-  findBeersByTemperatureRangeSortedByName(
-    temperature: number,
-  ): Promise<BeerDto[]>
-}
